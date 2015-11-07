fd144ef723e9ed6fc639ae99342291fb7ee16ca5
denix00 <denix00@gmail.com>
2015-11-07 04:14:28 +0100

    dodana prijava i registracija

--- app/src/main/java/hr/agrokor/tvz/denisglad/grupolovac/MainActivity.java
+++ app/src/main/java/hr/agrokor/tvz/denisglad/grupolovac/MainActivity.java
@@ -1,27 +1,36 @@
 package hr.agrokor.tvz.denisglad.grupolovac;
 
 import android.app.ActionBar;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
+import org.w3c.dom.Text;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private EditText password2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
     public void onClick (View v){
         Intent trgovine = new Intent(this, new TrgovineActivity().getClass());
@@ -30,7 +39,142 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void onClickPrijava(View v){
+
+        username = (EditText) findViewById(R.id.txtUsernamePr);
+        password = (EditText) findViewById(R.id.txtPasswordPr);
+
+        boolean validationError = false;
+        StringBuilder validationErrorMessage =
+                new StringBuilder("Greška: ");
+        if (isEmpty(username)) {
+            validationError = true;
+            validationErrorMessage.append("prazno korisničko ime");
+        }
+        if (isEmpty(password)) {
+            if (validationError) {
+                validationErrorMessage.append(" i ");
+            }
+            validationError = true;
+            validationErrorMessage.append("prazna lozinka");
+        }
+        validationErrorMessage.append(".");
+
+        // If there is a validation error, display the error
+        if (validationError) {
+            Toast.makeText(MainActivity.this, validationErrorMessage.toString(), Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // Set up a progress dialog
+        final ProgressDialog dlg = new ProgressDialog(MainActivity.this);
+        dlg.setTitle("Molim pričekajte.");
+        dlg.setMessage("Prijava u tijeku, trenutak molim");
+        dlg.show();
+        // Call the Parse login method
+        ParseUser.logInInBackground(username.getText().toString(), password.getText()
+                .toString(), new LogInCallback() {
+
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                dlg.dismiss();
+                if (e != null) {
+                    // Show the error message
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                } else {
+                    // Start an intent for the dispatch activity
+                    Intent intent = new Intent(MainActivity.this, Meduaktivnost.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
 
 
+    public void onClickRegistracija(View v){
 
+        username = (EditText) findViewById(R.id.txtUsernameReg);
+        password = (EditText) findViewById(R.id.txtPasswordReg);
+        password2 = (EditText) findViewById(R.id.txtPassword2Reg);
+
+        // Validate the sign up data
+        boolean validationError = false;
+        StringBuilder validationErrorMessage =
+                new StringBuilder("Greška: ");
+        if (isEmpty(username)) {
+            validationError = true;
+            validationErrorMessage.append("prazno korisničko ime");
+        }
+        if (isEmpty(password)) {
+            if (validationError) {
+                validationErrorMessage.append(" i ");
+            }
+            validationError = true;
+            validationErrorMessage.append("prazna lozinka");
+        }
+        if (!isMatching(password, password2)) {
+            if (validationError) {
+                validationErrorMessage.append(" i ");
+            }
+            validationError = true;
+            validationErrorMessage.append("lozinke nisu jednake");
+        }
+        validationErrorMessage.append(".");
+
+        // If there is a validation error, display the error
+        if (validationError) {
+            Toast.makeText(MainActivity.this, validationErrorMessage.toString(), Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // Set up a progress dialog
+        final ProgressDialog dlg = new ProgressDialog(MainActivity.this);
+        dlg.setTitle("Molim pričekajte");
+        dlg.setMessage("Registracija u tijeku, trenutak molim");
+        dlg.show();
+
+        // Set up a new Parse user
+        ParseUser user = new ParseUser();
+        user.setUsername(username.getText().toString());
+        user.setPassword(password.getText().toString());
+        // Call the Parse signup method
+        user.signUpInBackground(new SignUpCallback() {
+
+            @Override
+            public void done(ParseException e) {
+                dlg.dismiss();
+                if (e != null) {
+                    // Show the error message
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                } else {
+                    // Start an intent for the dispatch activity
+                    Intent intent = new Intent(MainActivity.this, Meduaktivnost.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+
+    private boolean isEmpty(EditText etText) {
+        if (etText.getText().toString().trim().length() > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean isMatching(EditText etText1, EditText etText2) {
+        if (etText1.getText().toString().equals(etText2.getText().toString())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
---
+++ app/src/main/java/hr/agrokor/tvz/denisglad/grupolovac/Meduaktivnost.java
@@ -0,0 +1,26 @@
+package hr.agrokor.tvz.denisglad.grupolovac;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.parse.ParseUser;
+
+/**
+ * Created by Denis on 7.11.2015..
+ */
+public class Meduaktivnost extends AppCompatActivity {
+
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        // ako je prijavljen, odi na trgovine
+        // inace se vrati na pocetak
+        if(ParseUser.getCurrentUser() != null) {
+            startActivity(new Intent(this, TrgovineActivity.class));
+        }else {
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+}
